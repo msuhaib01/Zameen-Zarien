82925e01604b327892f5b478332cccafdf1bca29
msuhaib01 <127488926+msuhaib01@users.noreply.github.com>
2024-12-19 15:40:53 -0500

    fixed web

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { I18nManager } from 'react-native';
+import { I18nManager } from "react-native";
 
 // Set RTL once
 I18nManager.allowRTL(true);
 I18nManager.forceRTL(true);
 
-import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './src/theme/theme';
+import React from "react";
+import AppNavigator from "./src/navigation/AppNavigator";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/theme/theme";
 
 export default function App() {
   return (
